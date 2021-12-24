7c20778a8339ffd3d18f4f3f7f27befc7d76d0d2
MasterPCFull <italocarvalho100@gmail.com>
2021-12-23 21:16:27 -0300

    melhorias Front end

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root to: "static_pages#index"
+
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contato', to: 'static_pages#contato'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
