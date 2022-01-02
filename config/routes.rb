fa10ce89c15a4f5867b98cce83d5b28fbea42242
MasterPCFull <italocarvalho100@gmail.com>
2022-01-02 17:50:11 -0300

    melhorias em user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   
-  resources :contacts
+  
+
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+
+# Resources
+
+resources :contacts
+resources :users, only: [ :new, :create ]
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
